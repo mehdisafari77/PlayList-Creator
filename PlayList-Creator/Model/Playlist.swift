148332286a0ce989de3b05e4baddd57990b59a6f
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-02-27 13:46:18 -0800

    create playlist mode

--- PlayList-Creator/Model/Playlist.swift
+++ PlayList-Creator/Model/Playlist.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+class Playlist: Codable {
+    let name: String
+    var songs: [Song]
+    
+    init (name: String, songs: [Song] = []) {
+        self.name = name
+        self.songs = songs
+    }
+}
+
+extension Playlist: Equatable {
+    static func == (lhs: Playlist, rhs: Playlist) -> Bool {
+        return lhs.name == rhs.name && lhs.songs == rhs.songs
+    }
+}
+
